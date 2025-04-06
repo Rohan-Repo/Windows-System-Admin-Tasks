8bf910c1f1f2bcddf3e628795c5146841d639218
Rohan <65016586+Rohan-Repo@users.noreply.github.com>
2025-04-05 23:44:50 -0400

    Adding Codes to connect to the SQL Server DB using Windows Auth & SQL Server Auth

---
+++ PowerShell-DB-Scripts/SQL-Server/Connect-To-SQL-Server/ConnectUsingSQLAuth.ps1
@@ -0,0 +1,27 @@
+# Create SQL Connection
+$sqlConn = New-Object System.Data.SqlClient.SqlConnection
+
+# Create the Connection String
+# I don't have an Instance Name Setup so ignore it and with SQL Server Authentication I need to specify the newly created User Credentials
+$sqlConn.ConnectionString = "Server=Rohan-XPS-15;Database=ADUserDB;User=SQLAdmin;Password=SQLAdmin"
+
+# Open the Connection
+$sqlConn.Open()
+
+Write-Host "DB Status : " $sqlConn.State -ForegroundColor Cyan
+
+Write-Host "DB Name : " $sqlConn.Database -ForegroundColor Magenta
+
+Write-Host "Server Name : " $sqlConn.DataSource -ForegroundColor Yellow
+
+# Unique GUID Created for every TimeOut Duration
+Write-Host "Connection ID : " $sqlConn.ClientConnectionId -ForegroundColor Green
+
+Write-Host "TimeOut : " $sqlConn.ConnectionTimeout
+
+Write-Host "Server Version : " $sqlConn.ServerVersion -ForegroundColor Blue
+
+# Close the Connection
+$sqlConn.Close()
+
+Write-Host "DB Status : " $sqlConn.State -ForegroundColor Red
