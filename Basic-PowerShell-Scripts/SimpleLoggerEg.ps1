efbd66af27e75dda4dcbb76de8b0770ca7116294
Rohan <65016586+Rohan-Repo@users.noreply.github.com>
2025-06-27 17:44:44 -0400

    Adding a Simple Logger which takes a msg and prints in different Styles

---
+++ Basic-PowerShell-Scripts/SimpleLoggerEg.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Enter a Message:"
+$msg = Read-Host 
+
+Write-Host "`n $(Get-Date)" : " ERROR - " $msg -ForegroundColor DarkRed -BackgroundColor Yellow
+
+Start-Sleep -Seconds 10 
+
+Write-Host "`n $(Get-Date)" : " WARNING - " $msg -ForegroundColor Black -BackgroundColor DarkGray
+
+Start-Sleep -Seconds 10 
+
+Write-Host "`n $(Get-Date)" : " INFO - " $msg -ForegroundColor Cyan -BackgroundColor Black
